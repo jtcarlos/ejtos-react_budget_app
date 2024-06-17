a42563a50fdd4bbf2d231f9046894034c79a1ec4
Thom Carlos <john.thomas.carlos0913@gmail.com>
2024-06-17 09:23:59 +0800

    added initial budget app code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,35 @@
+import React, { useContext } from "react"
+import { TiDelete } from "react-icons/ti"
 
+import { AppContext } from "../context/AppContext"
+
+const ExpenseItem = ({ id, name, cost }) => {
+  const { dispatch } = useContext(AppContext)
+
+  const handleDeleteExpense = () =>
+    dispatch({
+      type: "DELETE_EXPENSE",
+      payload: id,
+    })
+
+  const increaseAllocation = () =>
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: { name, cost: 10 },
+    })
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>£{cost}</td>
+      <td>
+        <button onClick={() => increaseAllocation(name)}>+</button>
+      </td>
+      <td>
+        <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
+      </td>
+    </tr>
+  )
+}
+
+export default ExpenseItem
